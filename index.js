972225245274dd91c038b8f5d38425639ff0e3fa
Your Name <you@example.com>
2022-12-07 19:24:03 +0300

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+//Challenge 1.
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" ,alt="";
+
+//Challenge 2.
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'; 
+
+//Challenge 3.
+<ul>
+<li><option value="a">a</option>
+<option value="b">b</option>
+ <option value="c">c</option>
+ <option value="d">d</option>
+</li>
+</ul>
+
+
+//Challenge 4.
+
+
